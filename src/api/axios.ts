3d5d4e0cb76c99c2d545f4fd0f1fa40dd35ba3f1
ysujii <suji072443@gmail.com>
2026-05-24 19:01:30 +0900

    chore: 쿠키 세션 가능하도록 설정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
   // 방금 .env에 적어둔 주소를 자동으로 불러옵니다.
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
