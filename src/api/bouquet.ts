2e861ebbdfec79f787778f7fcec04312ba89fc07
sandy <a26948077@gmail.com>
2026-05-24 23:44:53 +0900

    api 경로 수정

--- src/api/bouquet.ts
+++ src/api/bouquet.ts
@@ -2,6 +2,6 @@ import { api } from './axios'; // 2번에서 만든 파일을 불러옴
 
 export const createBouquet = async (body: unknown) => {
   // baseURL이 세팅되어 있으니 뒷부분 주소('/bouquets')만 씁니다.
-  const response = await api.post('/bouquets', body);
+  const response = await api.post('/api/bouquets', body);
   return response.data;
 };
