7a8a0872f7e5a7ff532af8b183e6afcac03f2666
sandy <a26948077@gmail.com>
2026-05-25 15:01:48 +0900

    feat: 카톡 공유시 줄바꿈 수정4

--- src/pages/modalPage.tsx
+++ src/pages/modalPage.tsx
@@ -140,7 +140,9 @@ export default function ModalPage({ onClose, bouquetData }: ModalPageProps) {
       if (navigator.canShare && navigator.canShare({ files: [file] })) {
         await navigator.share({
           files: [file],
+          title: "FLOWERTOWB",
           text: "나만의 커스터마이징 플라워, 플라워토브 🌸",
+          url: "https://lotowb.com",
         });
       } else {
         // PC 접속이거나 지원하지 않는 브라우저일 경우 예외 처리
